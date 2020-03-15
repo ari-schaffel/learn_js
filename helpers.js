1de99ede827c8baad8051175d15fd547b8311e1e
ari-schaffel <yaschaffel@gmail.com>
2020-03-14 23:23:44 -0400

    cleaned up files

--- helpers.js
+++ helpers.js
@@ -6,17 +6,12 @@ const {
 
 
 var log ={
-add:function add(a,b){
-    return a+b
-},
 timteit:async function timeit(func){
     t0=performance.now()
     await func()
     t1=performance.now()
     console.log((t1-t0)/1000)
-
 }
 }
 
 module.exports = log
-// module.exports = log
